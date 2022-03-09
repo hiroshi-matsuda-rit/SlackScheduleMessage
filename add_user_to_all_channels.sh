042b657eaa71dcd953ecfd780e6cf6b24c3317ec
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2022-03-09 19:59:15 +0900

    set limit=1000

--- add_user_to_all_channels.sh
+++ add_user_to_all_channels.sh
@@ -4,7 +4,10 @@ USER_OAUTH_TOKEN=`cat user_oauth_token.txt`
 
 count=0
 
-curl -s -XPOST "https://slack.com/api/conversations.list" -H "Authorization: Bearer ${USER_OAUTH_TOKEN}" | \
+curl -s -XPOST "https://slack.com/api/conversations.list" \
+  -H "Authorization: Bearer ${USER_OAUTH_TOKEN}" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d '{"limit":1000}' | \
   sed -r 's/[{]"ok":true,"channels":.//' | \
   sed -r 's/.,"response_metadata":.+/\n/' | \
   sed -r 's/},{/}\n{/g' | \
--- show_scheduled_messages.sh
+++ show_scheduled_messages.sh
@@ -2,7 +2,10 @@
 
 USER_OAUTH_TOKEN=`cat user_oauth_token.txt`
 
-curl -s -XPOST "https://slack.com/api/chat.scheduledMessages.list" -H "Authorization: Bearer ${USER_OAUTH_TOKEN}" | \
+curl -s -XPOST "https://slack.com/api/chat.scheduledMessages.list" \
+  -H "Authorization: Bearer ${USER_OAUTH_TOKEN}" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d '{"limit":1000}' | \
   sed -r 's/[{]"ok":true,"scheduled_messages":.//' | \
   sed -r 's/.,"response_metadata":.+//' | \
   sed -r 's/},{/}\n{/g'
